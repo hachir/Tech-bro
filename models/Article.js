a0171ad50de47200a0dfeba63e812f328ad3db4e
hachir <ha1991ha@yahoo.com>
2019-10-12 16:30:29 -0600

    Article.js

---
+++ models/Article.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the schema for the Articles
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    summary: String,
+    link: {
+        type: String,
+        required: true
+    },
+    imageUrl: String,
+    saved: {
+        type: Boolean,
+        default: false
+    },
+    // An article can have many notes
+    // The ref property links the ObjectId to the Note model
+    notes: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }]
+});
+
+// Create a model from the schema
+const Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
